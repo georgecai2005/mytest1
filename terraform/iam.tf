73e2ee30c108a618ae123d4e2b1c9ebcc968f61c
george2005 <georgecai2005@gmail.com>
2024-03-28 11:50:54 -0400

    added new GSA

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,11 +1,11 @@
-resource "google_service_account" "github_action" {
-  account_id   = "github2"
-  display_name = "github2"
+resource "google_service_account" "github_action3" {
+  account_id   = "github3"
+  display_name = "github3"
 }
 
 resource "google_project_iam_member" "github_execution_role" {
   project = var.project_id
-  role    = "roles/editor" # Update with the desired role for GitHub execution
+  role    = "roles/viewer" # Update with the desired role for GitHub execution
 
-  member = "serviceAccount:${google_service_account.github_action.email}"
+  member = "serviceAccount:${google_service_account.github_action3.email}"
 }
